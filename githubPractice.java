3ec09ead2e4018b76e984e02c75904f863ca6b61
CPaulMorrey <56007942+CPaulMorrey@users.noreply.github.com>
2022-09-27 17:06:42 -0600

    Create githubPractice.java
    Initial Fall 2022 commit.

---
+++ githubPractice.java
@@ -0,0 +1,52 @@
+// githubPractice.java
+// Build command: javac githubPractice.java
+
+public class githubPractice {
+    public static void main(String[] args) {
+        System.out.println("IT 6780 - GitHub Practice");
+        System.out.println("The IT 6780 class is filled with many awesome programmers.  Some of them are:");
+
+/*
+
+Command line version of the activity
+------------------------------------
+BEFORE YOU START... you need to have git installed on your system.
+
+Instructions: 
+               Clone a local repository to your system.
+                    git clone https://github.com/CPaulMorrey/IT6780.git
+                    cd IT6780
+                    
+               Test the program
+                    javac githubPractice.java
+                    java githubPractice
+                    
+               Edit the source code of the program.  Remove the two forward slashes (comment marker) on the line with your name.
+               
+               Test the program to make sure it prints your name (if possible).
+                    javac githubPractice.java
+                    java githubPractice
+                    git diff
+               
+               Commit your changes.
+                    git commit githubPractice.java -m "Added myself"
+                    git push https://github.com/CPaulMorrey/IT6780.git 
+               
+               After the rest of the class has arrived at this point then all names should be listed by the running program.
+               Update your local repository and it should pull in the changes that other students have made.
+                    git pull
+               
+*/
+//        System.out.println("Acacia");
+//        System.out.println("Eurico");
+//        System.out.println("Jacob");
+//        System.out.println("Jared");
+//        System.out.println("Johnson");
+//        System.out.println("Joshten");
+//        System.out.println("Logan");
+        System.out.println("Paul");
+//        System.out.println("Skyler");
+//        System.out.println("Tyler");
+//        System.out.println("William");
+    }
+}
